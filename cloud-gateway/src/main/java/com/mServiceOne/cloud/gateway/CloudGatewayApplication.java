da656357e12891265e56534d488fb1f729c0c6fb
Md Shafiqul Haque <shafiqulhaque1971@gmail.com>
2023-01-31 12:37:37 -0600

    hystrix add and setup
    hystrix add and setup

--- cloud-gateway/src/main/java/com/mServiceOne/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/mServiceOne/cloud/gateway/CloudGatewayApplication.java
@@ -3,10 +3,11 @@ package com.mServiceOne.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
+@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
